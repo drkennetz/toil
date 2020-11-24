d72d6af0a71b4d9f3fdbcaa943f89894acf6dc83
Dennis Kennetz <dennis.kennetz@stjude.org>
2020-11-24 12:14:07 -0600

    updating to upstream

--- src/toil/batchSystems/lsf.py
+++ src/toil/batchSystems/lsf.py
@@ -116,7 +116,7 @@ class LSFBatchSystem(AbstractGridEngineBatchSystem):
             if '.' in lsfJobID:
                 job, task = lsfJobID.split('.', 1)
 
-            meminfo = self.parseMaxMem(job)
+            self.parseMaxMem(job)
             # first try bjobs to find out job state
             if check_lsf_json_output_supported:
                 args = ["bjobs", "-json", "-o",
@@ -325,7 +325,10 @@ class LSFBatchSystem(AbstractGridEngineBatchSystem):
             except subprocess.CalledProcessError as err:
                 logger.debug("[job ID %s] Unable to collect maximum memory usage: %s",
                              str(jobID), str(err))
+<<<<<<< HEAD
             return None
+=======
+>>>>>>> d66cb8a80eea4fac0273129998cad5b795030c5a
 
     def getWaitDuration(self):
         """We give LSF a second to catch its breath (in seconds)"""
